c290cf986538ca7318e64636c8196d206aa435dd
Markus Scherer <markus.f.scherer@gmail.com>
2020-12-23 15:48:42 +0100

    Cleaned up 19.2

--- 19/19.hs
+++ 19/19.hs
@@ -14,36 +14,10 @@ data Rule = Const Char | Ref Int | Seq [Rule] | Alt Rule Rule deriving (Show, Eq
 type Parser = Parsec Void String
 
 ruleToParser :: M.Map Int Rule -> Rule -> Parser String
-ruleToParser m (Const c) = do
-  c' <- char c
-  return [c]
-ruleToParser m (Ref i) = do
-  let r = fromJust $ M.lookup i m
-  ruleToParser m r
-ruleToParser m (Seq rs) = do
-  r <- mapM (ruleToParser m) rs
-  return $ concat r
+ruleToParser m (Const c) = string [c]
+ruleToParser m (Ref i) = ruleToParser m (fromJust $ M.lookup i m)
+ruleToParser m (Seq rs) = liftM concat $ mapM (ruleToParser m) rs
 ruleToParser m (Alt r1 r2) = try (ruleToParser m r1) <|> try (ruleToParser m r2)
-
-showRule (Const c) = '"' : c : "\""
-showRule (Ref i) = "ref " ++ show i
-showRule (Alt r1 r2) = "(" ++ showRule r1 ++ " | " ++ showRule r2 ++ ")"
-showRule (Seq rs) = unwords $ map showRule rs
-
-generateStrings :: Rule -> [String]
-generateStrings (Const c) = [[c]]
-generateStrings (Ref i) = ["(" ++ show i ++ ")"]
-generateStrings (Alt r1 r2) = nub $ do
-  s1 <- generateStrings r1
-  s2 <- generateStrings r2
-  [s1, s2]
-generateStrings (Seq rs) = nub $ map concat $ mapM generateStrings rs
-
-inlineRefs :: M.Map Int Rule -> [Int] -> Rule -> Rule
-inlineRefs m ex r@(Ref i) = if i `elem` ex then r else inlineRefs m ex (fromJust $ M.lookup i m)
-inlineRefs m ex (Seq rs) = Seq $ map (inlineRefs m ex) rs
-inlineRefs m ex (Alt r1 r2) = Alt (inlineRefs m ex r1) (inlineRefs m ex r2)
-inlineRefs m ex (Const c) = Const c
 
 lexeme :: Parser a -> Parser a
 lexeme = L.lexeme (void (many (char ' ')))
@@ -58,10 +32,10 @@ main = do
   let parser31 = ruleToParser ruleMap $ fromJust $ M.lookup 31 ruleMap
   let parserf = do
         a <- count' 2 10000 parser42
-        b <- count' 1 10000 parser31
+        b <- count' 1 (length a - 1) parser31
         return (a, b)
   print $ length $ filter isJust $ map (parseMaybe parser) words
-  print $ length $ filter (\(a, b) -> length a > length b) $ mapMaybe (parseMaybe parserf) words
+  print $ length $ mapMaybe (parseMaybe parserf) words
 
 parseConst :: Parser Rule
 parseConst = lexeme $ between (char '"') (char '"') ((char 'a' <|> char 'b') >>= (return . Const))
