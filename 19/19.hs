29f18cee295f90ae78d220f4115260379da5320c
Markus Scherer <markus.f.scherer@gmail.com>
2020-12-23 12:40:14 +0100

    Solved 19.1

---
+++ 19/19.hs
@@ -0,0 +1,69 @@
+import Control.Monad
+import Control.Monad.Combinators.Expr
+import qualified Data.Map as M
+import Data.Maybe
+import Data.Void
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as L
+
+data Rule = Const Char | Ref Int | Seq [Rule] | Alt Rule Rule deriving (Show, Eq)
+
+type Parser = Parsec Void String
+
+ruleToParser :: M.Map Int Rule -> Rule -> Parser String
+ruleToParser m (Const c) = do
+  c' <- char c
+  return [c]
+ruleToParser m (Ref i) = do
+  let r = fromJust $ M.lookup i m
+  ruleToParser m r
+ruleToParser m (Seq rs) = do
+  r <- mapM (ruleToParser m) rs
+  return $ concat r
+ruleToParser m (Alt r1 r2) = try (ruleToParser m r1) <|> try (ruleToParser m r2)
+
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme (void (many (char ' ')))
+
+main = do
+  input <- getContents
+  let (ruleLines, words) = fromJust $ parseMaybe parseInput input
+  let ruleMap = M.fromList ruleLines
+  let parser = ruleToParser ruleMap $ fromJust $ M.lookup 0 ruleMap
+  print $ length $ filter isJust $ map (parseMaybe parser) words
+
+parseConst :: Parser Rule
+parseConst = lexeme $ between (char '"') (char '"') ((char 'a' <|> char 'b') >>= (return . Const))
+
+parseRef :: Parser Rule
+parseRef = lexeme $ L.decimal >>= (return . Ref)
+
+parseSeq :: Parser Rule
+parseSeq = lexeme $ count' 2 10000 parseRef >>= (return . Seq)
+
+parseSimple :: Parser Rule
+parseSimple = parseConst <|> try parseSeq <|> try parseRef
+
+parseAlt :: Parser Rule
+parseAlt = lexeme $ do
+  r1 <- parseSimple
+  _ <- lexeme (char '|')
+  Alt r1 <$> parseSimple
+
+parseRule :: Parser Rule
+parseRule = try parseAlt <|> parseSimple
+
+parseRuleLine :: Parser (Int, Rule)
+parseRuleLine = do
+  i <- lexeme L.decimal
+  _ <- lexeme (char ':')
+  r <- parseRule
+  return (i, r)
+
+parseInput :: Parser ([(Int, Rule)], [String])
+parseInput = do
+  rs <- endBy parseRuleLine eol
+  _ <- eol
+  ws <- endBy (many (char 'a' <|> char 'b')) eol
+  return (rs, ws)
