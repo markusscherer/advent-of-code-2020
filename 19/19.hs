cbb6cffd3d03ef8f249b141dd2d6d7f212b4c9e3
Markus Scherer <markus.f.scherer@gmail.com>
2020-12-23 15:46:43 +0100

    Solved 19.2

--- 19/19.hs
+++ 19/19.hs
@@ -1,11 +1,13 @@
 import Control.Monad
 import Control.Monad.Combinators.Expr
+import Data.List
 import qualified Data.Map as M
 import Data.Maybe
 import Data.Void
 import Text.Megaparsec
 import Text.Megaparsec.Char
 import qualified Text.Megaparsec.Char.Lexer as L
+import Text.Megaparsec.Debug
 
 data Rule = Const Char | Ref Int | Seq [Rule] | Alt Rule Rule deriving (Show, Eq)
 
@@ -23,6 +25,26 @@ ruleToParser m (Seq rs) = do
   return $ concat r
 ruleToParser m (Alt r1 r2) = try (ruleToParser m r1) <|> try (ruleToParser m r2)
 
+showRule (Const c) = '"' : c : "\""
+showRule (Ref i) = "ref " ++ show i
+showRule (Alt r1 r2) = "(" ++ showRule r1 ++ " | " ++ showRule r2 ++ ")"
+showRule (Seq rs) = unwords $ map showRule rs
+
+generateStrings :: Rule -> [String]
+generateStrings (Const c) = [[c]]
+generateStrings (Ref i) = ["(" ++ show i ++ ")"]
+generateStrings (Alt r1 r2) = nub $ do
+  s1 <- generateStrings r1
+  s2 <- generateStrings r2
+  [s1, s2]
+generateStrings (Seq rs) = nub $ map concat $ mapM generateStrings rs
+
+inlineRefs :: M.Map Int Rule -> [Int] -> Rule -> Rule
+inlineRefs m ex r@(Ref i) = if i `elem` ex then r else inlineRefs m ex (fromJust $ M.lookup i m)
+inlineRefs m ex (Seq rs) = Seq $ map (inlineRefs m ex) rs
+inlineRefs m ex (Alt r1 r2) = Alt (inlineRefs m ex r1) (inlineRefs m ex r2)
+inlineRefs m ex (Const c) = Const c
+
 lexeme :: Parser a -> Parser a
 lexeme = L.lexeme (void (many (char ' ')))
 
@@ -31,7 +53,15 @@ main = do
   let (ruleLines, words) = fromJust $ parseMaybe parseInput input
   let ruleMap = M.fromList ruleLines
   let parser = ruleToParser ruleMap $ fromJust $ M.lookup 0 ruleMap
+  -- the following three parsers were generated after manual inspection
+  let parser42 = ruleToParser ruleMap $ fromJust $ M.lookup 42 ruleMap
+  let parser31 = ruleToParser ruleMap $ fromJust $ M.lookup 31 ruleMap
+  let parserf = do
+        a <- count' 2 10000 parser42
+        b <- count' 1 10000 parser31
+        return (a, b)
   print $ length $ filter isJust $ map (parseMaybe parser) words
+  print $ length $ filter (\(a, b) -> length a > length b) $ mapMaybe (parseMaybe parserf) words
 
 parseConst :: Parser Rule
 parseConst = lexeme $ between (char '"') (char '"') ((char 'a' <|> char 'b') >>= (return . Const))
