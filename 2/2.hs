ff44c6c9b18a6aeee2afe59b40d824aa9cd8704e
Markus Scherer <markus.f.scherer@gmail.com>
2020-12-25 19:23:58 +0100

    Cleaned up 2

--- 2/2.hs
+++ 2/2.hs
@@ -1,43 +1,35 @@
+import Control.Monad
 import Data.Void
 import Text.Megaparsec
 import Text.Megaparsec.Char
 import qualified Text.Megaparsec.Char.Lexer as L
 
---import Text.Megaparsec.String
 type Parser = Parsec Void String
 
 data PasswordData = P Int Int Char String deriving (Show)
 
-sc :: Parser ()
-sc =
-  L.space
-    space1 -- (2)
-    (L.skipLineComment "//") -- (3)
-    (L.skipBlockComment "/*" "*/") -- (4)
-
-lexeme :: Parser a -> Parser a
-lexeme = L.lexeme sc
-
-valid :: PasswordData -> Bool
-valid (P a b c s) =
+valid1 :: PasswordData -> Bool
+valid1 (P a b c s) =
   let count = length $ filter (== c) s
    in count >= a && count <= b
 
 valid2 :: PasswordData -> Bool
 valid2 (P a b c s) =
   let b1 = s !! (a - 1) == c
-   in let b2 = s !! (b - 1) == c
-       in b1 /= b2
+      b2 = s !! (b - 1) == c
+   in b1 /= b2
 
 main = do
   input <- getContents
   let (Just x) = parseMaybe (many parsePasswordData) input
-  putStr $ show $ length $ filter valid2 x
+  print $ length $ filter valid1 x
+  print $ length $ filter valid2 x
+
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme (void (many (char ' ')))
 
 integer :: Parser Int
 integer = lexeme L.decimal
-
-lower = choice $ map char "abcdefghijklmnopqrstuvwxyz"
 
 parsePasswordData :: Parser PasswordData
 parsePasswordData = do
@@ -45,8 +37,8 @@ parsePasswordData = do
   _ <- char '-'
   b <- integer
   --  _ <- char ' '
-  c <- lower
+  c <- lowerChar
   _ <- string ": "
-  s <- many lower
+  s <- many lowerChar
   _ <- newline
   return (P a b c s)
